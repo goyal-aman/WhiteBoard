85bd446d356b794aa1172546b859432addf6c2dd
goyal-aman <goyal.amanrocks@gmail.com>
2021-06-03 19:58:11 +0530

    added functionality to draw to canvas

--- public/script.js
+++ public/script.js
@@ -1,11 +1,35 @@
 let canvas = document.querySelector('#canvas');
 
-let {top:canvasTop} = canvas.getBoundingClientRect();
-canvas.height = window.innerHeight - canvasTop; 
+let { top: canvasTop } = canvas.getBoundingClientRect();
+canvas.height = window.innerHeight - canvasTop;
 canvas.width = window.innerWidth;
 
-window.addEventListener("resize", function(){
-    canvas.height = window.innerHeight - canvasTop; 
+window.addEventListener("resize", function () {
+    canvas.height = window.innerHeight - canvasTop;
     canvas.width = window.innerWidth;
-    
+
+})
+
+let ctx = canvas.getContext("2d");
+let isMouseDown = false;
+
+canvas.addEventListener('mousedown', function (e) {
+    isMouseDown = true;
+    let x = e.clientX;
+    let y = e.clientY - canvasTop;
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+})
+
+canvas.addEventListener('mouseup', function (e) {
+    isMouseDown = false;
+})
+
+canvas.addEventListener('mousemove', function (e) {
+    if (isMouseDown) {
+        let x = e.clientX;
+        let y = e.clientY - canvasTop;
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
 })
