2e034f97484ab0d8c64108c2a5a6baa973c53a9a
goyal-aman <goyal.amanrocks@gmail.com>
2021-06-03 21:40:58 +0530

    added show and hide functionality tool options

--- public/tools.js
+++ public/tools.js
@@ -1,22 +1,38 @@
 let pencil = document.querySelector("#pencil");
 let eraser = document.querySelector("#eraser");
 
+let pencilOptions = pencil.querySelector('.tool-options');
+let eraserOptions = eraser.querySelector('.tool-options');
+
 let activeTool = "pencil";
 
-pencil.addEventListener("click", function(){
-    if(activeTool=="pencil"){
+pencil.addEventListener("click", function () {
+    if (activeTool == "pencil") {
         // open pencil open and close.
-    }else{
+        if (pencilOptions.classList.contains("hide")) {
+            pencilOptions.classList.remove("hide");
+        } else {
+            pencilOptions.classList.add("hide");
+        }
+
+    } else {
         activeTool = "pencil";
         ctx.strokeStyle = "black";
+        eraserOptions.classList.add("hide");
     }
 });
 
-eraser.addEventListener("click", function(){
-    if(activeTool=="eraser"){
+eraser.addEventListener("click", function () {
+    if (activeTool == "eraser") {
+        if (eraserOptions.classList.contains("hide")) {
+            eraserOptions.classList.remove("hide");
+        } else {
+            eraserOptions.classList.add("hide");
+        }
 
-    }else{
+    } else {
         activeTool = "eraser";
-        ctx.strokeStyle = "white"; 
+        ctx.strokeStyle = "white";
+        pencilOptions.classList.add("hide");
     }
 })
