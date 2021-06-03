00e6dad1afbec9fc1d70fd7f822cbc8a0fb2ed72
goyal-aman <goyal.amanrocks@gmail.com>
2021-06-03 22:38:36 +0530

    improved switching behaviour between pencil and eraser

--- public/tools.js
+++ public/tools.js
@@ -4,7 +4,22 @@ let eraser = document.querySelector("#eraser");
 let pencilOptions = pencil.querySelector('.tool-options');
 let eraserOptions = eraser.querySelector('.tool-options');
 
+let pencilSizeInput = pencil.querySelector("input");
+let eraserSizeInput = eraser.querySelector("input");
+
 let activeTool = "pencil";
+
+// adding event listeners in all pencil colors options.
+let currentPencilColor = "black";
+let pencilColors = document.querySelectorAll(".pencil-colors div");
+for(let i=0; i<pencilColors.length; i++){
+    pencilColors[i].addEventListener("click", function(e){
+        console.log(e);
+        let selectedPencilColor = e.target.className;
+        ctx.strokeStyle = selectedPencilColor;
+        currentPencilColor = selectedPencilColor;
+    })
+}
 
 pencil.addEventListener("click", function () {
     if (activeTool == "pencil") {
@@ -17,7 +32,9 @@ pencil.addEventListener("click", function () {
 
     } else {
         activeTool = "pencil";
-        ctx.strokeStyle = "black";
+        ctx.strokeStyle = currentPencilColor
+        // set line width equal to vlaue of pencil rannge input value.
+        ctx.lineWidth = pencilSizeInput.value;
         eraserOptions.classList.add("hide");
     }
 });
@@ -33,6 +50,19 @@ eraser.addEventListener("click", function () {
     } else {
         activeTool = "eraser";
         ctx.strokeStyle = "white";
+
+        // set lineWidth equal to vlaue of eraser range input value.
+        ctx.lineWidth = eraserSizeInput.value;
         pencilOptions.classList.add("hide");
     }
+})
+
+pencilSizeInput.addEventListener("change", function(){
+    let newPencilSize = pencilSizeInput.value
+    ctx.lineWidth = newPencilSize;
+})
+
+eraserSizeInput.addEventListener("change", function(){
+    let newEraserSize = eraserSizeInput.value;
+    ctx.lineWidth = newEraserSize;
 })
